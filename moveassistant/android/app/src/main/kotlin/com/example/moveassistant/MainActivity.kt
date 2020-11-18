bcc193231c5c9d264dcae4f572408d887a2ac3d0
AndreHabib <andrehabib@ufrn.edu.br>
2020-11-18 15:14:11 -0300

    Icone and SplashScreen

---
+++ moveassistant/android/app/src/main/kotlin/com/example/moveassistant/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.moveassistant
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ moveassistant/lib/Colors.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+const BackGroundColor = Color(0xFF8257E5);
+const TextColor = Color(0xFF101010);
+const WhiteColor = Color(0xFFF0F0F7);
